d335c2173573bbd93d98e11d87e5e991f7b0619f
Julio Cesar <js.cesar42@gmail.com>
2025-03-29 12:09:02 -0300

    Project started

---
+++ ios-swinject-example-swiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ios-swinject-example-swiftUI
+//
+//  Created by Julio Cesar Pereira on 29/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios-swinject-example-swiftUI/ios_swinject_example_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_swinject_example_swiftUIApp.swift
+//  ios-swinject-example-swiftUI
+//
+//  Created by Julio Cesar Pereira on 29/03/25.
+//
+
+import SwiftUI
+
+@main
+struct ios_swinject_example_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
